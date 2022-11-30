ee42f91525d480a4c48c678fdaa2841a83e96131
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2022-11-30 19:18:27 +0100

    Add Model Checkpoint & Data loading

--- vae/training.py
+++ vae/training.py
@@ -10,6 +10,7 @@ import datetime
 import wandb
 from wandb.keras import WandbCallback
 import os
+import jax
 
 
 from model import Sampling, get_encoder, get_decoder, VAE
@@ -51,7 +52,39 @@ optimizer = keras.optimizers.Adam(
 
 vae.compile(optimizer=optimizer)
 
-#Load Data here
+
+
+
+print("Load Data")
+
+with h5py.File(config_dict["training_data"], 'r') as F:
+  labels = np.array(F['ans'])
+  # reject some we don't care about, keep the rest
+  (milkywaylikes_idx,) = np.where(labels > 3)
+  images = np.array(F['images'][milkywaylikes_idx])
+  # Milky way similar galaxies are class '3' ('-4' normalizes the labels again)
+  labels = np.array(F['ans'][milkywaylikes_idx]) - 4
+
+images = images.astype(np.float32) / 255.
+
+# crop the image a bit to get 64x64 shape
+def crop_center(images, cropx, cropy):
+    _, y, x, _ = images.shape
+    startx = x // 2 -(cropx // 2)
+    starty = y // 2 -(cropy // 2)    
+    return images[:, starty:starty+cropy, startx:startx+cropx, :]
+
+images = crop_center(images=images, cropx=64, cropy=64)
+
+# shuffle once
+rng, key = jax.random.split(rng)
+shuffle_idx = jax.random.randint(key, (len(images),), 0, len(images))
+images = images[shuffle_idx, ...]
+labels = labels[shuffle_idx, ...]
+
+# show shape
+print("Data Shape:",images.shape)
+
 
 
 #set up the callbacks
@@ -66,10 +99,20 @@ os.makedirs(plot_dir, exist_ok=True)
 
 print("All Functions and Classes defined! Start with the Training!")
 
-history = vae.fit(X,
+
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=save_dir,
+    save_weights_only=True,
+    monitor='loss',
+    mode='min',
+    save_best_only=True)
+
+
+history = vae.fit(images,
     verbose=2,
     epochs=config_dict["epochs"],
     callbacks=[
         WandbCallback(),
+        model_checkpoint_callback
         ],
     )
