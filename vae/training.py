c7742dbe835109fc31b2439dbb5ac2fddc44c7a2
Henning Schulze Eißing <h_schu55@uni-muenster.de>
2022-11-30 19:58:36 +0100

    Test Fix

--- vae/training.py
+++ vae/training.py
@@ -16,6 +16,14 @@ import jax
 from model import Sampling, get_encoder, get_decoder, VAE
 
 
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+
 print("Using tensorflow verion:", tf.__version__)
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
