1234ebcfec826b4fb34eac5bf44a781628d7e7a0
Quizznor <paul.filip@protonmail.com>
2022-11-30 21:27:14 +0100

    Load image to numpy support

--- binaries.py
+++ binaries.py
@@ -1,8 +1,42 @@
 import matplotlib.pyplot as plt
-import tensorflor.keras as tf
+from PIL import Image
 import numpy as np
+import h5py
+
 
 # use Pythagorean norm (maybe weighted features in future?)
 def calculate_distance(features_1, features_2):
     return np.linalg.norm(features_1, features_2)
+
+def load_training_dataset():
+
+    import h5py
+    global labels, images
+
+    with h5py.File('Galaxy10.h5', 'r') as F:    
+
+        labels = np.array(F['ans'])
+        (milkywaylikes_idx,) = np.where(labels > 3)
+        images = np.array(F['images'][milkywaylikes_idx])
+        labels = np.array(F['ans'][milkywaylikes_idx]) - 4
+        images = images.astype(np.float32) / 255.
+
+
+def plot_class_distances(NN, reference_image_str):
+
+    try:
+        images == None
+    except NameError:
+        load_training_dataset()
+
+    classwise_distances = [[] for _ in range(np.unique(labels))]
+
+    # reference_image = np.array(Image.open(reference_image_str))
+    # reference_latent_position = NN.encoder(reference_image)
+
+
+
+    # for image in images:
+
+# if __name__ == "__main__":
 
